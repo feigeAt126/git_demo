9671597864a84270faf48a7f0ae19b96c4ade9fa
feigeAt126 <feige41032@126.com>
2022-09-15 17:02:51 +0800

    add getter/setter to Person.java

--- Person.java
+++ Person.java
@@ -1,4 +1,20 @@
 public Class Person {
 	String name;
 	int age;
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setAge(int age){
+		this.age = age;
+	}
+
+	public int getAge(){
+		return this.age;
+	}
 }
